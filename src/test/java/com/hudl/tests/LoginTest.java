4ac396cdb362d1a491ca4a892963b65bf8870d84
parveenkhan1 <parveensultanauk@gmail.com>
2025-09-07 21:25:22 +0100

    Initial commit: added LoginTest with proper Maven structure

---
+++ src/test/java/com/hudl/tests/LoginTest.java
@@ -0,0 +1,32 @@
+package com.hudl.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\amjed\\HUDL\\hudl-automation\\java\\drivers\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void openHudlHomePage() {
+        driver.get("https://www.hudl.com/");
+        System.out.println("Title is: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
